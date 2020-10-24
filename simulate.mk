df56a307912aa03667c99cd147046c91611b3760
June Tate-Gans <june@theonelab.com>
2020-10-24 00:47:34 -0500

    simulate: qemu refinements

--- simulate.mk
+++ simulate.mk
@@ -6,11 +6,15 @@ $(EMMC):
 
 simulate: $(OUT)/vice-embedded.img $(EMMC)
 	qemu-system-x86_64 \
+		-nodefaults \
 		-enable-kvm \
 		-m size=2048 \
 		-bios /usr/share/qemu/OVMF.fd \
-		-drive file=$(EMMC),if=ide,format=raw,unit=0 \
-		-drive file=$(OUT)/vice-embedded.img,if=ide,format=raw,unit=1 \
+		-usb \
+		-drive file=$(OUT)/vice-embedded.img,if=none,id=stick,format=raw \
+		-device usb-storage,drive=stick \
+		-drive file=$(EMMC),if=ide,format=raw \
+		-vga virtio \
 		-display gtk
 
 reset-hd:
