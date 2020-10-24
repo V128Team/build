8299ebe16f0695ba05d3a3c9f1f94a20ef016cc7
June Tate-Gans <june@theonelab.com>
2020-10-23 21:15:36 -0500

    prereqs: Remove unused prereqs.

--- prereqs.mk
+++ prereqs.mk
@@ -2,7 +2,5 @@ PACKAGES := devscripts equivs fakeroot whalebuilder
 
 prereqs:
 	sudo apt-get install $(PACKAGES)
-	sudo mk-build-deps --install $(ROOTDIR)/packages/vice/debian/control
-	whalebuilder create -r testing debian:testing
 
 .PHONY:: prereqs
