c58e98197a49f106dc9c1eda17bfcc75214607de
June Tate-Gans <june@theonelab.com>
2020-10-24 00:25:27 -0500

    rootfs: Ensure image deps work properly

--- rootfs.mk
+++ rootfs.mk
@@ -1,4 +1,4 @@
-DEPS    := $(BUILD)/rootfs.mk $(BUILD)/Makefile
+DEPS    := $(BUILD)/rootfs.mk
 SOURCES := $(shell find $(ROOTDIR)/distro -type f)
 DEBOS   := $(shell which debos)
 IMAGE   := $(OUT)/vice-embedded.img
@@ -7,12 +7,13 @@ ifeq ($(DEBOS),)
 $(error The debos tool could not be found -- please install and try again)
 endif
 
-$(OUT)/debos: $(SOURCES)
+$(OUT)/debos: $(SOURCES) $(OUT)/apt/Packages
 	mkdir -p $(OUT)/debos
 	cp -r $(ROOTDIR)/distro/* $(OUT)/debos/
 	cp -r $(OUT)/apt $(OUT)/debos/apt
 
-$(IMAGE): $(DEPS) | $(OUT) $(OUT)/debos
+$(IMAGE): $(DEPS) $(SOURCES) $(OUT)/apt/Packages $(OUT)/debos | $(OUT)
+	rm -f $(IMAGE)
 	cd $(OUT)/debos && $(DEBOS) \
 		-t image:$(IMAGE) \
 		--artifactdir=$(OUT)/debos \
