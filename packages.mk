773a512bf47dab84b9bcec0b0bea81ca6bfb0e96
June Tate-Gans <june@theonelab.com>
2020-10-25 15:56:02 -0500

    Integrate the v128-shell package into the build.

--- packages.mk
+++ packages.mk
@@ -8,6 +8,7 @@ include $(BUILD)/package_build.mk
 
 $(eval $(call build_package,vice,amd64))
 $(eval $(call build_package,plymouth-v128-theme,all))
+$(eval $(call build_package,v128-shell,all))
 
 clean::
 	rm -rf $(OUT)/packages
