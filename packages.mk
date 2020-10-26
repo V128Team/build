32f606f936b97c418306b5bd0e4830be1d4ebab4
June Tate-Gans <june@theonelab.com>
2020-10-26 02:35:21 -0500

    Add in the v128-shell package again

--- packages.mk
+++ packages.mk
@@ -10,7 +10,7 @@ $(eval $(call build_package,vice,amd64))
 $(eval $(call build_package,plymouth-v128-theme,all))
 $(eval $(call build_package,libwld,amd64))
 $(eval $(call build_package,libswc,amd64,libwld))
-#$(eval $(call build_package,v128-shell,amd64,libswc libwld))
+$(eval $(call build_package,v128-shell,amd64,libswc libwld))
 
 clean::
 	rm -rf $(OUT)/packages
