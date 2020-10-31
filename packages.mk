678d6f6e3479411fa0880076321dbb83287b263c
June Tate-Gans <june@theonelab.com>
2020-10-31 15:24:48 -0500

    Remove libwld and friends

--- packages.mk
+++ packages.mk
@@ -8,9 +8,7 @@ include $(BUILD)/package_build.mk
 
 $(eval $(call build_package,vice,amd64))
 $(eval $(call build_package,plymouth-v128-theme,all))
-$(eval $(call build_package,libwld,amd64))
-$(eval $(call build_package,libswc,amd64,libwld))
-$(eval $(call build_package,v128-shell,amd64,libswc libwld))
+$(eval $(call build_package,v128-shell,amd64))
 
 clean::
 	rm -rf $(OUT)/packages
