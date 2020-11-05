92861cd290981f0c36313484ceddb1f76e59936d
June Tate-Gans <june@theonelab.com>
2020-11-05 01:00:23 -0600

    Add in pi4-firmware as a package.

--- packages.mk
+++ packages.mk
@@ -9,6 +9,7 @@ include $(BUILD)/package_build.mk
 $(eval $(call build_package,vice,amd64))
 $(eval $(call build_package,plymouth-v128-theme,all))
 $(eval $(call build_package,v128-shell,amd64))
+$(eval $(call build_package,pi4-firmware,all))
 
 clean::
 	rm -rf $(OUT)/packages
