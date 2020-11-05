30f76ed2fda0a6a5a13c51a2ebe496b060e2990b
June Tate-Gans <june@theonelab.com>
2020-11-05 01:17:49 -0600

    Adding in the rpi-eeprom package.

--- packages.mk
+++ packages.mk
@@ -10,6 +10,7 @@ $(eval $(call build_package,vice,amd64))
 $(eval $(call build_package,plymouth-v128-theme,all))
 $(eval $(call build_package,v128-shell,amd64))
 $(eval $(call build_package,pi4-firmware,all))
+$(eval $(call build_package,rpi-eeprom,all))
 
 clean::
 	rm -rf $(OUT)/packages
