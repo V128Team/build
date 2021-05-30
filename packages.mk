881b081ac9752a1ca3a44b00194f7d8575268e73
June Tate-Gans <june@theonelab.com>
2021-05-30 15:45:51 -0500

    Update pi4-firmware to armhf

--- packages.mk
+++ packages.mk
@@ -9,7 +9,7 @@ include $(BUILD)/package_build.mk
 $(eval $(call build_package,vice,armhf))
 $(eval $(call build_package,plymouth-v128-theme,all))
 $(eval $(call build_package,v128-shell,armhf))
-$(eval $(call build_package,pi4-firmware,all))
+$(eval $(call build_package,pi4-firmware,armhf))
 $(eval $(call build_package,rpi-eeprom,all))
 $(eval $(call build_package,mdt-services,all))
 
